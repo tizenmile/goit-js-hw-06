b1b42870d6be8418a033c3faf3fd5a382c48ad35
tizenmile <tizen48@gmail.com>
2022-07-28 11:36:39 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -3,20 +3,20 @@ const button = document.querySelector(".login-form button");
 const email = document.getElementsByName("email")[0];
 const password = document.getElementsByName("password")[0];
 
-button.addEventListener("click", () => {
-  if (email.value === "" || password.value === "") {
-    alert("Заповніть всі поля");
-  }
-});
+button.addEventListener("click", () => {});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const isSubmited = {
-    elements: ({ 
-        login: email.value, 
-        password: password.value 
-    }),
+    elements: {
+      login: email.value,
+      password: password.value,
+    },
   };
-  console.log(isSubmited.elements);
-  document.querySelector(".login-form").reset();
+  if (email.value === "" || password.value === "") {
+    alert("Заповніть всі поля");
+  } else {
+    console.log(isSubmited.elements);
+    document.querySelector(".login-form").reset();
+  }
 });
