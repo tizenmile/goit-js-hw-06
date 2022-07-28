9c5b8115e9e3eab5a4d88e739966887ad5c67cfd
tizenmile <tizen48@gmail.com>
2022-07-28 11:36:29 +0300

    Виправлення ДЗ 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,8 @@
 const countUl = document.querySelectorAll(".item");
 
-console.log(`\nNumber of category: ${countUl.length}`);
+console.log(`Number of category: ${countUl.length}`);
 
-console.log(`\nCategory: ${countUl[0].children[0].innerText}`);
-
-console.log(`Elements: ${countUl[0].children[1].children.length}`);
-
-console.log(`\nCategory: ${countUl[1].children[0].innerText}`);
-
-console.log(`Elements: ${countUl[1].children[1].children.length}`);
-
-console.log(`\nCategory: ${countUl[2].children[0].innerText}`);
-
-console.log(`Elements: ${countUl[2].children[1].children.length}`);
+const allCategoriesArray = countUl.forEach((element) => {
+  console.log(`Category: ${element.querySelector("h2").textContent}`);
+  console.log(`Elements: ${element.querySelectorAll("li").length}`);
+});
