590ce5ab24f2ef16d2841de9f2705de971ddc9e4
tizenmile <tizen48@gmail.com>
2022-07-28 11:36:34 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -7,37 +7,13 @@ const ingredients = [
   "Condiments",
 ];
 const newEl = document.querySelector("#ingredients");
-for (let index = 0; index < ingredients.length; index++) {
+
+const addElement = ingredients.map((element) => {
   const liEl = document.createElement("li");
   liEl.classList.add("item");
-  liEl.textContent = ingredients[index];
-  newEl.append(liEl);
-}
+  liEl.textContent = element;
+  return liEl;
+});
+
+newEl.append(...addElement);
 console.log(newEl);
-
-// const liEl = document.createElement("li");
-// liEl.classList.add("item");
-// liEl.textContent = ingredients[0];
-
-// const liEl1 = document.createElement("li");
-// liEl1.classList.add("item");
-// liEl1.textContent = ingredients[1];
-
-// const liEl2 = document.createElement("li");
-// liEl2.classList.add("item");
-// liEl2.textContent = ingredients[2];
-
-// const liEl3 = document.createElement("li");
-// liEl3.classList.add("item");
-// liEl3.textContent = ingredients[3];
-
-// const liEl4 = document.createElement("li");
-// liEl4.classList.add("item");
-// liEl4.textContent = ingredients[4];
-
-// const liEl5 = document.createElement("li");
-// liEl5.classList.add("item");
-// liEl5.textContent = ingredients[5];
-
-// newEl.append(liEl, liEl1, liEl2, liEl3, liEl4, liEl5);
-// console.log(newEl);
